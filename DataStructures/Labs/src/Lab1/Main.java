ccbc560c478a3edde2dfac06a63a41dcc24519cf
David <dguerrero@learnvest.com>
2017-07-03 12:23:56 -0400

    fixes folder hierarchy

---
+++ DataStructures/Labs/src/Lab1/Main.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Cauliflower c1 = new Cauliflower("Orange");
+		System.out.println(c1.isEdible());
+		System.out.println(c1.getFlowerColor());
+
+	}
+
+}
