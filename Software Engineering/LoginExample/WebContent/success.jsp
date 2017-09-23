0ee6aa1a4f8d5b2a5b1763de5fb08a196ff6da72
David <dguerrero@learnvest.com>
2017-09-23 07:34:59 -0400

    Adds all Software Engineering Eclipse projects to repository

---
+++ Software Engineering/LoginExample/WebContent/success.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Success Page</title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<%
+		String username = (String) request.getAttribute("Username");
+ 		String firstName = (String) request.getAttribute("firstName");
+		String lastName = (String) request.getAttribute("lastName");
+		String address = (String) request.getAttribute("address");
+		String phoneNumber = (String) request.getAttribute("phoneNumber");
+		String personalEmail = (String) request.getAttribute("personalEmail"); 
+
+		if (username != null) {
+	%>
+	<h1>
+		Welcome, <%=username%>! <br>
+	</h1>
+	<%
+		}
+		if (firstName != null){
+	%>
+			<h1>
+			Sign up complete. Your account information is displayed below: <br>
+			Your first name is <%=firstName%> <br>
+			Your last name is <%=lastName%> <br>
+			Your address is <%=address%> <br>
+			Your phone number is <%=phoneNumber%> <br>
+			Your personal email is <%=personalEmail%> <br> 
+			</h1>
+	<% 
+		}
+	%>
+	
+	<a href="login.jsp">Return to Login Page</a>
+</body>
+</html>
---
+++ Software Engineering/LoginExample/src/com/LoginServlet.java
@@ -0,0 +1,64 @@
+package com;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// default constructor
+	public LoginServlet() {
+
+	}
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("Username");
+		String password = request.getParameter("Password");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String address = request.getParameter("address");
+		String phoneNumber = request.getParameter("phoneNumber");
+		String personalEmail = request.getParameter("personalEmail");
+		
+
+		if (username.equalsIgnoreCase("Admin") && password.equalsIgnoreCase("Admin")) {
+			RequestDispatcher rd = request.getRequestDispatcher("success.jsp");
+			request.setAttribute("Username", username);
+			request.setAttribute("Password", password);
+			rd.forward(request, response);
+		} else if (firstName != null) {
+			request.setAttribute("Username", username);
+			request.setAttribute("Password", password);
+			request.setAttribute("firstName", firstName);
+			request.setAttribute("lastName", lastName);
+			request.setAttribute("address", address);
+			request.setAttribute("phoneNumber", phoneNumber);
+			request.setAttribute("personalEmail", personalEmail);
+			RequestDispatcher rd = request.getRequestDispatcher("success.jsp");
+			rd.forward(request, response);
+		}
+		 else {
+			response.sendRedirect("error.jsp");
+		}
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+}
