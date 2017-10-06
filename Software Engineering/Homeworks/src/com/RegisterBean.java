e608b6a5da88d6dce239681b618c0766145e64f3
David <dguerrero@learnvest.com>
2017-10-06 01:28:50 -0400

    inserts data into database

--- Software Engineering/Homeworks/src/com/RegisterBean.java
+++ Software Engineering/Homeworks/src/com/RegisterBean.java
@@ -1,5 +1,10 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -9,18 +14,8 @@ public class RegisterBean {
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String phoneNumber;
 	private String username;
 	private String password;
-	private String address;
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
 
 	public String getFirstName() {
 		return firstName;
@@ -45,15 +40,7 @@ public class RegisterBean {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
-	public String getPhoneNum() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNum(String number) {
-		this.phoneNumber = number;
-	}
-
+	
 	public String getUserName() {
 		return username;
 	}
@@ -68,5 +55,46 @@ public class RegisterBean {
 
 	public void setPassWord(String password) {
 		this.password = password;
+	}
+	
+	public String getById() {
+		Connection con = null;
+		try {
+			// Setup the DataSource object
+			com.mysql.jdbc.jdbc2.optional.MysqlDataSource ds = new com.mysql.jdbc.jdbc2.optional.MysqlDataSource();
+			ds.setServerName("localhost");
+			ds.setPortNumber(3306);
+			ds.setDatabaseName("online_course");
+			ds.setUser("root");
+			ds.setPassword("12345");
+
+			// Get a connection object
+			con = ds.getConnection();
+
+			// Get a prepared SQL statement
+//			String sql = "SELECT first_name from user where id = ?";
+//			String sql = "INSERT INTO user(firstName, lastName, email, userID, username, password) VALUES(?,?,?,?,?,?)";
+			String sql = "INSERT INTO user(first_name, last_name, email_address, username, password) VALUES(?,?,?,?,?);";
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1, this.firstName);
+			st.setString(2, this.lastName);
+			st.setString(3, this.email);
+			st.setString(4, this.username);
+			st.setString(5, this.password);
+			
+
+			// Execute the statement
+			int rs = st.executeUpdate();
+
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+			}
+		}
+		return "successreg";
+//		 return "successreg?faces-redirect=true";
 	}
 }
